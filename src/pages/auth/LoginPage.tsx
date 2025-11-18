cfd4150d4eb9eec0eb71911d7cf0125488968c76
aqib mansoor <itzaqib248@gmail.com>
2025-11-18 19:53:33 +0500

    Error Updated!

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -14,10 +14,10 @@ interface Credential {
 
 //  Updated predefined demo users with names
 const predefinedUsers: Credential[] = [
-  { email: "patient@example.com", password: "123", role: "patient", name: "Umer Khan" },
+  { email: "patient@example.com", password: "12345", role: "patient", name: "Umer Khan" },
   { email: "abc@gmail.com", password: "123", role: "patient", name: "Fahad Sohail" },
   { email: "aaa@gmail.com", password: "123", role: "patient", name: "Asad Khan" },
-  { email: "ayesha@hospital.com", password: "123", role: "doctor", name: "Dr. Ayesha Khan" },
+  { email: "ayesha@hospital.com", password: "12345", role: "doctor", name: "Dr. Ayesha Khan" },
   { email: "ali@hospital.com", password: "123", role: "doctor", name: "Dr. Ali Raza" },
   { email: "sara@hospital.com", password: "123", role: "doctor", name: "Dr. Sara Ahmed" },
   { email: "bilal@hospital.com", password: "123", role: "doctor", name: "Dr. Bilal Shah" },
